62abff95216757d79b5d2f51fca326d644180d47
Eko Rudianto <67891954+Xvae27@users.noreply.github.com>
2020-10-22 14:37:27 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a6000.mk
+    $(LOCAL_DIR)/lineage_a33f.mk
 
 # Lunch choices
 COMMON_LUNCH_CHOICES := \
-    lineage_a6000-userdebug
+    lineage_a33f-userdebug
